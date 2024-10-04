86ded0c6ad76db9fa9b2ff629d78921426a73e6a
harok <doriantheblue@naver.com>
2024-10-04 15:05:44 +0900

    https://leetcode.com/problems/climbing-stairs/

---
+++ leetcode/climbing-stairs/answer.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun climbStairs(n: Int): Int {
+        if (n <= 2) {
+            return n
+        }
+
+        val dp = IntArray(n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for (i in 3..n) {
+            dp[i] = dp[i - 2] + dp[i - 1]
+        }
+
+        return dp[n]
+    }
+}
