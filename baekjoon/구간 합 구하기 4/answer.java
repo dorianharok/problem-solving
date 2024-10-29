b976c6d7f372fa4b87680a43347795eee691ac10
harok <doriantheblue@naver.com>
2024-10-30 01:14:29 +0900

    https://www.acmicpc.net/problem/11659

---
+++ baekjoon/구간 합 구하기 4/answer.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int[] sum = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<=N; i++) {
+            sum[i] = sum[i-1] + arr[i-1];
+        }
+
+        while(M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            System.out.println(sum[j] - sum[i-1]);
+        }
+    }
+}
