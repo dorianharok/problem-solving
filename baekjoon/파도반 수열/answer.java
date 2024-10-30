74b634f16f93158d6ed49555a43153c5e674d51f
harok <doriantheblue@naver.com>
2024-10-30 17:06:58 +0900

    https://www.acmicpc.net/problem/9461

---
+++ baekjoon/파도반 수열/answer.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3; i<=100; i++) {
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+
+        while(T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
