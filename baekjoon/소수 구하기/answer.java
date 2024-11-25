4c6b50a3b3e5a60d98a11d0bd755d833b1a83a9f
harok <doriantheblue@naver.com>
2024-11-25 17:47:54 +0900

    https://www.acmicpc.net/problem/1929

---
+++ baekjoon/소수 구하기/answer.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = start; i <= end; i++) {
+            if(isPrime(i)) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static boolean isPrime(int n) {
+        if(n == 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
