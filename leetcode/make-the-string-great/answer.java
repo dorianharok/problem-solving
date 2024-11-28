d4c3f06f1590c52aa081b2abf0c6b459448b4bd9
harok <doriantheblue@naver.com>
2024-11-29 01:24:25 +0900

    https://leetcode.com/problems/make-the-string-great/

---
+++ leetcode/make-the-string-great/answer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String makeGood(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty()) {
+                if (stack.peek() - c == 32 || c - stack.peek() == 32) {
+                    stack.pop();
+                    continue;
+                }
+            }
+
+            stack.push(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+}
