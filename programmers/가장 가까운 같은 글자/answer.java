f7a7d87a35bb25693a2c64790a5299e5e07d8fa6
harok <doriantheblue@naver.com>
2025-04-16 23:26:14 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/142086

---
+++ programmers/가장 가까운 같은 글자/answer.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        int[] alpha = new int[26];
+        Arrays.fill(alpha, -1);
+
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(alpha[c-'a'] == -1) {
+                answer[i] = -1;
+            } else {
+                answer[i] = i - alpha[c-'a'];
+            }
+
+            alpha[c-'a'] = i;
+        }
+
+        return answer;
+    }
+}
