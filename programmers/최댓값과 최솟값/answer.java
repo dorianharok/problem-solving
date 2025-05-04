25bcf89f69c4813f1d401096eaca54a6e85f7846
harok <doriantheblue@naver.com>
2025-05-04 23:21:00 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/12939

---
+++ programmers/최댓값과 최솟값/answer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        int[] arr = Arrays.stream(s.split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        return arr[0] + " " + arr[arr.length - 1];
+    }
+}
