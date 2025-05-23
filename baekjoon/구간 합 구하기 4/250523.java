ca98412ea5b1d6f35b5427cf4e66b61c6409c167
harok <doriantheblue@naver.com>
2025-05-23 22:06:58 +0900

    https://www.acmicpc.net/problem/11659

---
+++ baekjoon/구간 합 구하기 4/250523.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N + 1];
+        for(int i=1; i<=N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken()) + arr[i-1];
+        }
+
+        while(M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            System.out.println(arr[j] - arr[i-1]);
+        }
+    }
+}
