0a448e350499342fcf9f4c5dcd95175007401139
harok <doriantheblue@naver.com>
2025-05-25 23:35:33 +0900

    https://leetcode.com/problems/missing-number/

---
+++ leetcode/missing-number/answer.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun missingNumber(nums: IntArray): Int {
+        var num = nums.size
+        for (i in nums.indices) {
+            num = num xor i xor nums[i]
+        }
+        return num
+    }
+}
