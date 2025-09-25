4e7958b923154773fc720ba29ed7680d20f6b809
harok <doriantheblue@naver.com>
2025-09-25 10:08:54 +0900

    https://leetcode.com/problems/3sum/

---
+++ leetcode/3sum/answer.kt
@@ -0,0 +1,31 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        nums.sort()
+
+        val op = mutableListOf<List<Int>>()
+        for(i in nums.indices) {
+            if(i > 0 && nums[i] == nums[i - 1]) continue
+
+            val target = -nums[i]
+            var left = i + 1
+            var right = nums.size - 1
+            while(left < right) {
+                val currentSum = nums[left] + nums[right]
+                if(currentSum == target) {
+                    op.add(listOf(nums[i], nums[left], nums[right]))
+                    while (left < right && nums[left] == nums[left + 1]) left++
+                    while (left < right && nums[right] == nums[right - 1]) right--
+
+                    left++
+                    right--
+                } else if (currentSum < target) {
+                    left++
+                } else {
+                    right--
+                }
+            }
+        }
+
+        return op
+    }
+}
