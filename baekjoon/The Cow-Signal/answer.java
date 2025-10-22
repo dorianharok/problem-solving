c8817034dfff363edbfb2669e8f95e4ee0165d6a
harok <doriantheblue@naver.com>
2025-10-22 19:19:30 +0900

    https://www.acmicpc.net/problem/14175

---
+++ baekjoon/The Cow-Signal/answer.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < K; j++) {
+                for (int k = 0; k < M; k++) {
+                    for(int l = 0; l < K; l++) {
+                        sb.append(str.charAt(k));
+                    }
+                }
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
