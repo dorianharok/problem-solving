b23ebac32de01b8a05b58f8438583d87a813fd97
harok <doriantheblue@naver.com>
2025-10-27 10:44:11 +0900

    https://leetcode.com/problems/coin-change-ii/

---
+++ leetcode/coin-change-ii/answer.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun change(amount: Int, coins: IntArray): Int {
+        val dp = IntArray(amount + 1)
+        dp[0] = 1
+
+        for(coin in coins) {
+            for(i in coin..amount) {
+                dp[i] += dp[i - coin]
+            }
+        }
+
+        return dp[amount]
+    }
+}
